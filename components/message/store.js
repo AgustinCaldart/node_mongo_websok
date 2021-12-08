6dc042bc9dab7880f3845c05cd5266103c7c2041
acaldart <caldart123@gmail.com>
2021-12-08 13:03:21 -0300

    conexion a mongo + get y add message

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,21 @@
-const list = [];
+const { connect } = require('mongoose');
+const Model = require('./model');
 
+const CONECTOR =
+  'mongodb+srv://root:root123@message.nmbvy.mongodb.net/message?retryWrites=true&w=majority';
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 function addMessage(message) {
-  list.push(message);
+  /* list.push(message); */
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessage() {
-  return list;
+async function getMessage() {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
@@ -15,3 +25,12 @@ module.exports = {
   //update
   //delete
 };
+
+connect(CONECTOR, OPTIONS, (MongoError) => {
+  // si algo sale mal mostramos el error y paramos el servidor
+  if (MongoError) {
+    console.error(MongoError);
+    process.exit(1);
+  }
+  console.log('Conectado con exito');
+});
